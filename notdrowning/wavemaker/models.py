1bba5f3ad1ed3e6fa0614c1e33178e4e87a77b0b
josh <joshsmith2@gmail.com>
2020-05-12 23:02:28 +0100

    Build models for waves, added shape option, freq and bit jon sez hi.

--- notdrowning/wavemaker/models.py
+++ notdrowning/wavemaker/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Wave(models.Model):
+    # TODO: Sensible maxes and mins: 20 to 2K
+    frequency = models.DecimalField(max_digits=5, decimal_places=3)
+    bitrate = models.PositiveIntegerField()
+
+    SINE = "SIN"
+    SQUARE = "SQU"
+    TRIANGLE = "TRI"
+    WAVE_CHOICES = [
+        (SINE, 'Sine'),
+        (SQUARE, 'Square'),
+        (TRIANGLE, 'Triangle'),
+    ]
+    shape = models.CharField(
+        max_length=3,
+        choices=WAVE_CHOICES,
+        default=SINE
+    )
+
+
+    # Other model logic - see https://www.digitalocean.com/community/tutorials/how-to-create-django-models
+    class Meta:
+        ordering = ['frequency','shape', 'bitrate']
